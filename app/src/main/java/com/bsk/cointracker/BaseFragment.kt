a801a45a7fda4ad527874a7a5ec79f3da6addeaa
Burak Karabulut <burak.karabulut@consultant.turkcell.com.tr>
2020-09-03 20:13:39 +0300

    BaseFragment.kt layoutId connected to base binding mechanism

---
+++ app/src/main/java/com/bsk/cointracker/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.bsk.cointracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bsk.cointracker.databinding.FragmentCoinsBinding
+import javax.inject.Inject
+
+abstract class BaseFragment : Fragment() {
+
+    @Inject
+    protected lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    @get:LayoutRes
+    abstract val layoutId: Int
+
+    protected lateinit var binding: ViewDataBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate<FragmentCoinsBinding>(
+            inflater, layoutId, container, false
+        ).apply {
+            lifecycleOwner = this@BaseFragment
+        }
+        return binding.root
+    }
+}
