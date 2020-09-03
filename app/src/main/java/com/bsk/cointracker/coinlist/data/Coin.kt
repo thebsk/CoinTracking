0e8e576c544de32fc20a51c26fb23208941bbacc
Burak Karabulut <burak.karabulut@consultant.turkcell.com.tr>
2020-09-03 22:23:13 +0300

    missing Coin parameters are added and TypeConverters are generated

--- app/src/main/java/com/bsk/cointracker/coinlist/data/Coin.kt
+++ app/src/main/java/com/bsk/cointracker/coinlist/data/Coin.kt
@@ -3,6 +3,7 @@ package com.bsk.cointracker.coinlist.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 @Entity(tableName = "coins")
 data class Coin(
@@ -12,7 +13,20 @@ data class Coin(
     @field:SerializedName("symbol")
     val symbol: String,
     @field:SerializedName("name")
-    val name: String
+    val name: String,
+    @field:SerializedName("price_change_percentage_24h")
+    val priceChangePercentage24H: String? = null,
+    @field:SerializedName("description")
+    val description: Map<String, String>? = null,
+    @field:SerializedName("image")
+    val image: Map<String, String>? = null,
+    @field:SerializedName("market_data")
+    val marketData: MarketData? = null
 ) {
     override fun toString(): String = name
 }
+
+data class MarketData(
+    @field:SerializedName("current_price")
+    val currentPrice: Map<String, String>? = null
+) : Serializable
--- app/src/main/java/com/bsk/cointracker/data/Converters.kt
+++ app/src/main/java/com/bsk/cointracker/data/Converters.kt
@@ -1,16 +1,35 @@
 package com.bsk.cointracker.data
 
 import androidx.room.TypeConverter
-import java.util.*
+import com.bsk.cointracker.coinlist.data.MarketData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 /**
  * Type converters to allow Room to reference complex data types.
  */
-class Converters {
+object Converters {
     @TypeConverter
-    fun calendarToDatestamp(calendar: Calendar): Long = calendar.timeInMillis
+    @JvmStatic
+    fun stringToMap(value: String?): Map<String, String>? {
+        return Gson().fromJson(value, object : TypeToken<Map<String, String>?>() {}.type)
+    }
 
     @TypeConverter
-    fun datestampToCalendar(value: Long): Calendar =
-        Calendar.getInstance().apply { timeInMillis = value }
+    @JvmStatic
+    fun mapToString(value: Map<String, String>?): String? {
+        return if (value == null) null else Gson().toJson(value)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun marketToString(value: MarketData?): String? {
+        return if (value == null) null else Gson().toJson(value)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToMarket(value: String?): MarketData? {
+        return Gson().fromJson(value, object : TypeToken<MarketData?>() {}.type)
+    }
 }
