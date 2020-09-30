d6885e3eca9f37c7ed060f8e7654ef6041bcb0a5
Burak Karabulut <burak.karabulut@consultant.turkcell.com.tr>
2020-09-30 11:32:08 +0300

    viewmodel - firestore integration is done

--- app/src/main/java/com/bsk/cointracker/ui/coinldetail/CoinViewModel.kt
+++ app/src/main/java/com/bsk/cointracker/ui/coinldetail/CoinViewModel.kt
@@ -3,6 +3,7 @@ package com.bsk.cointracker.ui.coinldetail
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bsk.cointracker.data.remote.entities.Coin
 import com.bsk.cointracker.data.remote.repository.CoinRepository
 
 
@@ -14,4 +15,10 @@ class CoinViewModel @ViewModelInject constructor(private val repository: CoinRep
     val coin by lazy {
         repository.observeCoin(viewModelScope, coinId)
     }
+
+    fun coinById(coinId: String) = repository.getFavoriteCoinById(coinId)
+
+    fun saveCoin(coin: Coin) = repository.saveFavoriteCoin(coin)
+
+    fun removeCoin(coin: Coin) = repository.deleteCoin(coin)
 }
