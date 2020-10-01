f403d63913ff998478e0c278294a4bb53dbd0e48
Burak Karabulut <burak.karabulut@consultant.turkcell.com.tr>
2020-10-02 02:15:42 +0300

    work manager custom factory adjustments are made for Hilt

--- app/src/main/java/com/bsk/cointracker/App.kt
+++ app/src/main/java/com/bsk/cointracker/App.kt
@@ -1,7 +1,20 @@
 package com.bsk.cointracker
 
 import android.app.Application
+import android.util.Log
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(Log.DEBUG)
+            .build()
+}
