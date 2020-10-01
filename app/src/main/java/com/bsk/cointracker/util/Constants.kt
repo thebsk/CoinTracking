48644216e231f7a9ff45854efa57887c8e0a9f41
Burak Karabulut <burak.karabulut@consultant.turkcell.com.tr>
2020-10-02 02:19:45 +0300

    constants of SyncCoinWorkerHelper.kt is added

--- app/src/main/java/com/bsk/cointracker/util/Constants.kt
+++ app/src/main/java/com/bsk/cointracker/util/Constants.kt
@@ -1,12 +1,25 @@
 package com.bsk.cointracker.util
 
 object Constants {
+    // Worker names
+    const val WORK_NAME_SYNC_COIN = "workSyncCoin"
+
     // Keys
-    const val KEY_USERS = "users"
     const val KEY_COINS = "coins"
     const val KEY_ID = "id"
     const val KEY_USER_ID = "userId"
+    const val KEY_COIN_ID = "keyCoinId"
+    const val KEY_COIN_NAME = "keyCoinName"
 
     // Request Codes
     const val REQUEST_CODE_SIGN_IN = 111
+
+    // Notification constants
+    @JvmField
+    val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
+        "Coin Tracker Notifications"
+    const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
+        "Shows notifications whenever coin sync work starts"
+    const val CHANNEL_ID = "COIN_NOTIFICATION"
+    const val NOTIFICATION_ID = 1
 }
